7dc603e6e2b545de66150c757497c8fcecc4717c
Viktor Zhabskyi <viktordevopscourse@gmail.com>
2023-12-07 22:12:11 +0200

    user docker file

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,22 +1,5 @@
 pipeline {
-    agent {
-        kubernetes {
-                   label 'my-kubernetes-agent'
-                   defaultContainer 'jnlp'
-                   yaml """
-                       apiVersion: v1
-                       kind: Pod
-                       metadata:
-                         labels:
-                           jenkins-agent: builder
-                       spec:
-                         containers:
-                         - name:  jenkins-agent-container
-                           image: quay.io/projectquay/golang:1.20
-                           tty: true
-                       """
-               }
-    }
+   agent { dockerfile true }
 
     parameters {
         choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
