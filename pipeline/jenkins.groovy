3f6826d27ed889d6820acca51016c2d4ca204ab0
Viktor Zhabskyi <viktordevopscourse@gmail.com>
2023-12-07 16:21:21 +0200

    update jenkins pipeline

--- pipeline/jenkins.groovy
+++ pipeline/jenkins.groovy
@@ -1,17 +1,36 @@
 pipeline {
     agent any
     parameters {
-
-        choice(name: 'OS', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
-
+        choice(name: 'OS', defaultValue:'linux', choices: ['linux', 'darwin', 'windows', 'all'], description: 'Pick OS')
+        choice(name: 'ARCH', defaultValue:'amd64',  choices: ['amd64', 'arm64'], description: 'Pick ARCH')
+        string(name: 'REGISTRY', defaultValue: 'docker.io/viktordevopscourse', description: 'Enter registry')
     }
+
+    environment {
+        TELE_TOKEN = credentials('TELEGRAM_TOKEN')
+        DOCKER_HUB_CREDENTIALS = credentials('dockerhub-credentials-id')
+    }
+
     stages {
-        stage('Example') {
+        stage('test') {
             steps {
-                echo "Build for platform ${params.OS}"
-
-                echo "Build for arch: ${params.ARCH}"
-
+                echo "Run unit tests"
+                sh 'make test'
+            }
+        }
+        stage('build') {
+            steps {
+                echo "Build for platform ${params.OS} arch: ${params.ARCH}"
+                sh "make image TARGETOS=${OS} TARGETARCH=${ARCH}"
+            }
+        }
+        stage('publish') {
+            steps {
+                echo "login to docker hub"
+                withCredentials([[$class: 'UsernamePasswordMultiBinding', credentialsId: 'DOCKER_HUB_CREDENTIALS', usernameVariable: 'DOCKER_HUB_USERNAME', passwordVariable: 'DOCKER_HUB_PASSWORD']]) {
+                    sh "docker login -u ${DOCKER_HUB_USERNAME} -p ${DOCKER_HUB_PASSWORD}"
+                }
+                sh "make push REGISTRY=${parems.REGISTRY} TARGETOS=${OS} TARGETARCH=${ARCH}"
             }
         }
     }
